e6226434664ad01f6d3830d46e861b34646e79ce
ballinyouup <bryanhuici@gmail.com>
2026-02-26 00:57:49 -0500

    fix: fixed json links

--- scripts/download.sh
+++ scripts/download.sh
@@ -8,24 +8,24 @@ mkdir -p "$PROJECT_ROOT/PGraphRAG"
 cd "$PROJECT_ROOT/PGraphRAG"
 
 # Amazon Reviews dataset for All Beauty Category.
-wget https://github.com/PGraphRAG-benchmark/PGraphRAG/blob/main/data_splits/amazon_train.json
-wget https://github.com/PGraphRAG-benchmark/PGraphRAG/blob/main/data_splits/amazon_dev.json
-wget https://github.com/PGraphRAG-benchmark/PGR-LLM/raw/refs/heads/main/data_splits/amazon_test.json
+wget https://raw.githubusercontent.com/PGraphRAG-benchmark/PGraphRAG/refs/heads/main/data_splits/amazon_train.json
+wget https://raw.githubusercontent.com/PGraphRAG-benchmark/PGraphRAG/refs/heads/main/data_splits/amazon_dev.json
+wget https://raw.githubusercontent.com/PGraphRAG-benchmark/PGR-LLM/refs/heads/main/data_splits/amazon_test.json
 
 # Hotel Reviews dataset from Kaggle.
-wget https://github.com/PGraphRAG-benchmark/PGraphRAG/blob/main/data_splits/hotel_train.json
-wget https://github.com/PGraphRAG-benchmark/PGraphRAG/blob/main/data_splits/hotel_dev.json
-wget https://github.com/PGraphRAG-benchmark/PGraphRAG/blob/main/data_splits/hotel_test.json
+wget https://raw.githubusercontent.com/PGraphRAG-benchmark/PGraphRAG/refs/heads/main/data_splits/hotel_train.json
+wget https://raw.githubusercontent.com/PGraphRAG-benchmark/PGraphRAG/refs/heads/main/data_splits/hotel_dev.json
+wget https://raw.githubusercontent.com/PGraphRAG-benchmark/PGraphRAG/refs/heads/main/data_splits/hotel_test.json
 
 # Grammar and Online Product Reviews dataset from Kaggle.
-wget https://github.com/PGraphRAG-benchmark/PGraphRAG/blob/main/data_splits/gap_train.json
-wget https://github.com/PGraphRAG-benchmark/PGraphRAG/blob/main/data_splits/gap_dev.json
-wget https://github.com/PGraphRAG-benchmark/PGraphRAG/blob/main/data_splits/gap_test.json
+wget https://raw.githubusercontent.com/PGraphRAG-benchmark/PGraphRAG/refs/heads/main/data_splits/gap_train.json
+wget https://raw.githubusercontent.com/PGraphRAG-benchmark/PGraphRAG/refs/heads/main/data_splits/gap_dev.json
+wget https://raw.githubusercontent.com/PGraphRAG-benchmark/PGraphRAG/refs/heads/main/data_splits/gap_test.json
 
 # B2W-Reviews dataset from github.
-wget https://github.com/PGraphRAG-benchmark/PGraphRAG/blob/main/data_splits/b2w_train.json
-wget https://github.com/PGraphRAG-benchmark/PGraphRAG/blob/main/data_splits/b2w_dev.json
-wget https://github.com/PGraphRAG-benchmark/PGraphRAG/blob/main/data_splits/b2w_test.json
+wget https://raw.githubusercontent.com/PGraphRAG-benchmark/PGraphRAG/refs/heads/main/data_splits/b2w_train.json
+wget https://raw.githubusercontent.com/PGraphRAG-benchmark/PGraphRAG/refs/heads/main/data_splits/b2w_dev.json
+wget https://raw.githubusercontent.com/PGraphRAG-benchmark/PGraphRAG/refs/heads/main/data_splits/b2w_test.json
 
 # Finetune: Download Ministral-3-3B-Instruct-2512 model from Huggingface.
 mkdir -p "$PROJECT_ROOT/models"
